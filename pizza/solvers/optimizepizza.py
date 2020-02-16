0e34e82020641b71bd94aecc84073a4b67fe5df5
Shane <shane@Shanes-MBP.home>
2020-02-16 14:47:06 -0500

    a suboptimal solver appears

---
+++ pizza/solvers/optimizepizza.py
@@ -0,0 +1,41 @@
+
+def binarysearch(intList, n):
+    """fimd largest value in sorted list smaller than n"""
+
+    if len(intList) == 1:
+        return intList[0]
+
+    midpoint = len(intList) // 2
+    if intList[midpoint] == n:
+        return n
+    else:
+        if n <= intList[midpoint]:
+            return binarysearch(intList[:midpoint], n)
+        else:
+            return binarysearch(intList[midpoint:], n)
+
+
+def partialsumfromlargest(sumList, candidateList, threshold, maxSum):
+    """Return the smallest values from the list and the sum of the largest values which surpasses the threshold ratio"""
+
+    if len(candidateList) == 0 or maxSum < candidateList[0]:
+        return sumList
+    elif maxSum == candidateList[0]:
+        sumList.append(candidateList[0])
+        return sumList
+
+    largeSumSize = maxSum*threshold  # identify target to reach with large array values only.
+    cutoffSize = binarysearch(candidateList, int(largeSumSize))  # largest candidate size which is smaller than target.
+    cutoffIndex = candidateList.index(cutoffSize)
+    numLargeElements = int(largeSumSize/cutoffSize)
+    largeElements = candidateList[cutoffIndex:cutoffIndex - numLargeElements:-1]
+    sumList.extend(largeElements)
+    newSum = maxSum - sum(largeElements)  # new max sum for recursion
+    newCandidateList = candidateList[0:cutoffIndex + 1 - numLargeElements]
+    return partialsumfromlargest(sumList, newCandidateList, threshold, newSum)
+
+
+M = 100
+pizzaList = [4, 14, 15, 18, 29, 32, 36, 82, 95, 95]
+pizzasoln = partialsumfromlargest([], pizzaList, .99, M)
+allsolns = [partialsumfromlargest([], pizzaList, 0.6 + j*0.01, M) for j in range(40)]
