8336130abc3623bc13b158fe23814dc162e6f577
reb <reboumans@gmail.com>
2020-02-16 13:47:45 -0500

    Prevent running out of pizza sizes giving an infite loop

--- pizza/solvers/basic.py
+++ pizza/solvers/basic.py
@@ -2,7 +2,7 @@ def solve(problem):
     total_slices = 0
     selected_pizzas = []
     pizza_sizes = list(enumerate(problem["pizza_sizes"]))
-    while problem["pizza_sizes"]:
+    while pizza_sizes:
         index, size = pizza_sizes.pop(0)
         if (size + total_slices) > problem["max_slices"]:
             return selected_pizzas
