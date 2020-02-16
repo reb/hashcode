9f4a202d9f41fd2c865cfecfc7ffb602b7c2ca4f
Elena <e.queirolo@vu.nl>
2020-02-16 15:05:21 -0500

    new simple solver : slightly_smarter Starts from end of list of pizzas to find optimal sum optimal not achieved due to target sum being lower than requested sum

---
+++ pizza/solvers/slightly_smarter.py
@@ -0,0 +1,22 @@
+import logging
+
+logger = logging.getLogger('slightly_smarter.py')
+
+def solve(problem):
+    target= .95*problem["max_slices"]
+    selected_pizzas, slices_taken, pizza_sizes = sub_solve(problem, target)
+    logger.debug(f"Solved with {slices_taken} slices taken.")
+    return selected_pizzas
+
+
+def sub_solve(problem, target):
+    total_slices = 0
+    selected_pizzas = []
+    pizza_sizes = list(enumerate(problem["pizza_sizes"]))
+    while pizza_sizes:
+        index, size = pizza_sizes.pop(-1)
+        if (size + total_slices) > target:
+            return selected_pizzas, total_slices, pizza_sizes
+        selected_pizzas.append(index)
+        total_slices += size
+    return selected_pizzas, total_slices, pizza_sizes
