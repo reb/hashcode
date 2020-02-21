c0f2e0c97654867b65fb3f42e3976bd386c62c2f
reb <reboumans@gmail.com>
2020-02-21 15:09:48 -0500

    Refactor problem with typed classes

--- books/problem.py
+++ books/problem.py
@@ -1,38 +1,89 @@
-def read(text):
+from typing import List
+
+
+class Library:
+    def __init__(
+        self, number_of_books: int, signup_days: int, capacity: int, book_ids: List[int]
+    ):
+        assert number_of_books == len(
+            book_ids
+        ), "The number of books does not equal the size of book_ids passed"
+        self.number_of_books = number_of_books
+        self.signup_days = signup_days
+        self.capacity = capacity
+        self.book_ids = book_ids
+
+    def remove_book(self, book_id: int):
+        if book_id in self.book_ids:
+            self.book_ids.remove(book_id)
+            self.number_of_books -= 1
+
+    def sort_books_by_value(self, books: List[int]):
+        self.book_ids.sort(key=lambda book_id: books[book_id], reverse=True)
+
+
+class Problem:
+    def __init__(
+        self,
+        number_of_books: int,
+        number_of_libraries: int,
+        number_of_days: int,
+        books: List[int],
+    ):
+        self.number_of_books = number_of_books
+        self.number_of_libraries = number_of_libraries
+        self.number_of_days = number_of_days
+        self.books = books
+        self.libraries = []
+
+    def append_library(self, library: Library):
+        self.libraries.append(library)
+
+    def book_value(self, book_id: int) -> int:
+        return self.book_ids[book_id]
+
+
+class ScanningLibrary:
+    def __init__(self, library_id: int):
+        self.library_id = library_id
+        self.book_ids: List[int] = []
+
+    def queue_book(self, book_id: int):
+        self.book_ids.append(book_id)
+
+
+class Solution:
+    def __init__(self):
+        self.scanning_queue: List = []
+
+    def queue_library(self, library_id: int):
+        self.scanning_order.append(ScanningLibrary(library_id))
+
+
+def read(text: str) -> Problem:
     lines = text.split("\n")
     [number_of_books, number_of_libraries, number_of_days] = [
         int(i) for i in lines[0].split(" ")
     ]
     books = [int(i) for i in lines[1].split(" ")]
-    libraries = []
+    problem = Problem(number_of_books, number_of_libraries, number_of_days, books)
 
-    for index in range(2, len(lines) - 1, 2):
+    for index in range(2, len(lines) - 2, 2):
         if not lines[index]:
             continue
-        [books_in_library, signup_days, shipping_capacity] = [
+        [books_in_library, signup_days, capacity] = [
             int(i) for i in lines[index].split(" ")
         ]
         book_ids = [int(i) for i in lines[index + 1].split(" ")]
 
-        libraries.append(
-            {
-                "books_in_library": books_in_library,
-                "signup_days": signup_days,
-                "shipping_capacity": shipping_capacity,
-                "book_ids": book_ids,
-            }
+        problem.append_library(
+            Library(books_in_library, signup_days, capacity, book_ids)
         )
 
-    return {
-        "number_of_books": number_of_books,
-        "number_of_libraries": number_of_libraries,
-        "number_of_days": number_of_days,
-        "books": books,
-        "libraries": libraries,
-    }
+    return problem
 
 
-def write(solution):
+def write(solution: Solution) -> str:
     # remove empty scanning queues
     solution = [item for item in solution if item["book_ids"]]
     result = f"{len(solution)}\n"
---
+++ books/test/test_problem.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+from books.problem import Library
+
+
+class TestLibrary(TestCase):
+    def test_remove_book(self):
+        library = Library(5, 2, 2, [4, 2, 6, 9, 10])
+
+        library.remove_book(6)
+        self.assertEqual(
+            library.number_of_books,
+            4,
+            "number_of_books is not updated after removing book_id 6",
+        )
+        self.assertEqual(
+            library.book_ids,
+            [4, 2, 9, 10],
+            "book_ids is not updated after removing book_id 6",
+        )
+
+        library.remove_book(1)
+        self.assertEqual(
+            library.number_of_books,
+            4,
+            "number_of_books changed after removing a book that's not in the library",
+        )
+        self.assertEqual(
+            library.book_ids,
+            [4, 2, 9, 10],
+            "book_ids changed after removing a book that's not in the library",
+        )
+
+    def test_sort_books_by_value(self):
+        library = Library(5, 2, 2, [3, 2, 5, 8, 10])
+        books = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        library.sort_books_by_value(books)
+        self.assertEqual(
+            library.book_ids,
+            [10, 8, 5, 3, 2],
+            "book_ids is not properly sorted by value",
+        )
