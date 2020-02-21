b7916523e3af6bb5af5e15f7beccf4e9a2495d4b
Elena <e.queirolo@vu.nl>
2020-02-20 19:28:53 -0500

    leaning  bit more

--- books/solvers/scanning_choices.py
+++ books/solvers/scanning_choices.py
@@ -1,80 +1,73 @@
-def scan_choice(chosen_libraries):
+from operator import itemgetter
 
-    scan_list =[[] for i in range(len(chosen_libraries)-1)]
-    all_books = []
+def scan_choice(chosen_libraries,values):
+
+    scan_list = {}
+
+    for j in chosen_libraries
+        if j["book_ids"] == []
+            chosen_libraries.pop(j)
+    
+    dict_of_books = {}
+    for library in chosen_libraries:
+        library_id = library["id"]
+        for book in library["book_ids"]:
+            if book in dict_of_books:
+                dict_of_books[book]["libraries"].append(library_id)
+                dict_of_books[book]["number_of_libraries"] +=1
+            else:
+                dict_of_books[book] = {"libraries":[library_id],"value": values[book],"book_id" = book,"number_of_libraries":1}
+
+    
     number_of_scans = 0
     for library in chosen_libraries
-        all_books = all_books.extend(library["book_ids"])
         number_of_scans += library["shipping_capacity"]
     
-    book_unique = uniquelist(all_books)
-    number_options = 0*book_unique
-    for j in range(len(book_unique)-1):
-        number_options[j] = all_books.count(book_unique[j])
+    remaining_scans = number_of_scans
     
-    # sort book unique in decreasing order w.r.t. the value and reshuffle number_options in the appropriate way
+    list_of_books = list(dict_of_books.values())
+    list_of_books.sort(key = itemgetter("value"))
     
-    remaining_scans  = number_of_scans
+    slice = list_of_books[0:remaining_scans]
     
-    while remaining_scans > 0 and len(book_unique)>0:
-        slice = number_options[0:remaining_scans]
+    while remaining_scans > 0 and len(list_of_books)>0:
         
-        unscannable_books = [i for i,x in enumerate(slice) if x==0]
-        for i in reversed(unscannable_books)
-            book_unique.pop(i)
-            number_options.pop(i)
+        #for book in slice:
+        #    if book["number_of_libraries"] < 1:
+        #        slice.pop(book)
+        #        list_of_books.pop(book)
+        #        slice.append(list_of_books[remaining_scans])
+
+
+        for book in slice:
+            if book["number_of_libraries"] == 1:
+                only_library = book["libraries"]
+                if chosen_libraries[only_library]["shipping_capacity"]-len(scan_lists[only_library]) < 1:
+                    slice.pop(book)
+                    list_of_books.pop(book)
+                    slice.append(list_of_books[remaining_scans])
+                else:
+                    scan_list[only_library].append(book["book_id"])
+                    remaining_scans -= 1
+                    slice.pop(book)
+                    list_of_books.pop(book)
         
-        slice = number_options[0:remaining_scans]
-        only_one_chance = [i for i,x in enumerate(slice) if x==1]
-        
-        for book in only_one_chance:
-            only_library = find_one_library(book, chosen_libraries)
-            if chosen_libraries[only_library]["shipping_capacity"]-len(scanned_lists[only_library]) < 1:
-                book_unique.pop(book)
-                number_options.pop(book)
-            else:
-                scan_list[only_library].append(books_unique[book])
-                remaining_scans -= 1
-                book_unique.pop(book)
-                number_options.pop(book)
-        
-        interesting_book = book_unique[0]
-        available_libraries = find_library(interesting_book,chosen_libraries)
-        best_library = 0
+        interesting_book = list_of_books[0]
+        available_libraries = interesting_book["libraries"]
+        best_library = available_libraries[0]
         best_condition = chosen_libraries[best_library]["shipping_capacity"] - len(scanned_lists[best_library])
         for j in range(len(available_libraries)-1):
             optimality_condition = chosen_libraries[j]["shipping_capacity"] - len(scanned_lists[j])
             if optimality_condition > best_condition:
                 best_library = j
-        scan_list[best_library].append(interesting_book)
-        remaining_scans -= 1
-        book_unique.pop(0)
-        number_options.pop(0)
-        
+                best_condition = optimality_condition
+        if best_condition > 0 :
+            scan_list[best_library].append(interesting_book)
+            remaining_scans -= 1
+            list_of_books.pop(book)
+        else :
+            slice.pop(book)
+            list_of_books.pop(book)
+            slice.append(list_of_books[remaining_scans])
 
     return scan_list
-
-
-def uniquelist(list):
-"""uniquify a list of lists"""
-    seen = []
-    result = []
-    for item in list:
-        if item in seen:
-            continue
-        seen.append(item)
-    return seen
-
-
-def find_one_library(book, list_libraries):
-    for j in range(len(list_libraries)-1):
-        if book in list_libraries[j]["book_ids"]:
-            return j
-    return false
-            
-def find_library(book, list_libraries):
-    good_libs = []
-    for j in range(len(list_libraries)-1):
-        if book in list_libraries[j]["book_ids"]:
-            good_libs.append[j]
-    return good_libs
