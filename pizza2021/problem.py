ced3d5e37057e7994a693fb4727fdba4474cfc12
reb <reboumans@gmail.com>
2021-02-21 14:10:20 -0500

    Set up pizza problem with classes

--- pizza2021/problem.py
+++ pizza2021/problem.py
@@ -1,18 +1,45 @@
-def read(text):
+from typing import List, Set
+
+
+class Pizza:
+    def __init__(self, id: int, ingredients: Set[str]):
+        self.id = id
+        self.ingredient = ingredients
+
+
+class Problem:
+    def __init__(
+        self, pizzas: List[Set[str]], t2: object, t3: object, t4: object,
+    ):
+        self.pizzas = [Pizza(i, ingredients) for i, ingredients in enumerate(pizzas)]
+        self.t2 = t2
+        self.t3 = t3
+        self.t4 = t4
+
+
+class Solution:
+    def __init__(self):
+        self.deliveries = {}
+
+    def add_delivery(self, team_size: int, pizzas: List[int]):
+        self.deliveries[team_size] = pizzas
+
+
+def read(text: str) -> Problem:
     lines = text.split("\n")
-    [_, T2, T3, T4] = [int(i) for i in lines[0].split()]
-    pizzas = [p.split()[1:] for p in lines[1:]]
-    return {"T2": T2, "T3": T3, "T4": T4, "pizzas": pizzas}
+    teams = [int(i) for i in lines[0].split()][1:]
+    pizzas = [set(p.split()[1:]) for p in lines[1:]]
+    return Problem(pizzas, *teams)
 
 
-def write(deliveries):
+def write(solution: Solution) -> str:
     """
     Write out the deliveries
-    
+
     Format: {team_size: [pizza_id, ...]}
     """
 
-    output_lines = f"{len(deliveries)}"
-    for team_size, pizzas in deliveries.items():
-        output_lines.append(f"{team_size} {' '.join(pizzas)}")
+    output_lines = [f"{len(solution.deliveries)}"]
+    for team_size, pizzas in solution.deliveries.items():
+        output_lines.append(f"{team_size} {' '.join(str(p) for p in pizzas)}")
     return "\n".join((output_lines))
--- pizza2021/solvers/example.py
+++ pizza2021/solvers/example.py
@@ -1,2 +1,13 @@
-def solve(problem):
-    return {}
+from pizza2021.problem import Solution, Problem
+
+
+def solve(problem: Problem) -> Solution:
+    solution = Solution()
+
+    pizzas = problem.pizzas
+    for team_size in (2, 3, 4):
+        if len(pizzas) > team_size:
+            solution.add_delivery(team_size, [p.id for p in pizzas[:team_size]])
+            pizzas = pizzas[team_size:]
+
+    return solution
