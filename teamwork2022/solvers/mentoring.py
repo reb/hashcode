4e2ee3b7edaf73420293006e27a3ecc71716026f
Elena <elenaquei@hotmail.it>
2022-02-24 22:03:38 +0100

    mentoring

--- teamwork2022/solvers/mentoring.py
+++ teamwork2022/solvers/mentoring.py
@@ -1,15 +1,19 @@
 from teamwork2022.problem import Solution, Problem, Project, Contributor, Skill
 import numpy as np
+import logging
+
+logger = logging.getLogger("mentoring.py")
 
 
 def mentor_contributors(contributors: list[Contributor], project: Project):
     if not bool_complete(contributors, project):
         return contributors
     for iter_role in range(len(project.roles)):
-        for iter_contributor_skill in range(len(contributors[iter_role].skills)):
-            if contributors[iter_role].skills[iter_contributor_skill].name == project.roles[iter_role].skills.name:
-                if contributors[iter_role].skills[iter_contributor_skill].level <= project.roles[iter_role].skills.level:
-                    contributors[iter_role].skills[iter_contributor_skill].level = contributors[iter_role].skills[iter_contributor_skill].skills.level + 1
+        skill_name = project.roles[iter_role].name
+        skill_cont = contributors[iter_role].skills[skill_name]
+        if skill_cont.level <= project.roles[iter_role].level:
+            logger.debug(f"{skill_name}")
+            contributors[iter_role].skills[skill_name].level += 1
     return contributors
 
 
@@ -19,20 +23,15 @@ def bool_complete(contributors: list[Contributor], project: Project):
     if n_contr != n_roles:
         return False
     for iter in range(n_roles):
-        skill = project.roles[iter].skills
-        for iter_skill_cont in contributors[iter].skills:
-            if contributors[iter_skill_cont].skills.name == skill.name:
-                if contributors[iter].skills.level >= skill.level:
-                    break
-                if contributors[iter].skills.level < skill.level - 1:
-                    return False
-                if find_a_mentor(contributors, skill):
-                    break
-                else:
-                    return False
-            else:
-                if skill.level > 1:
-                    return False
+        skill = project.roles[iter]
+        if contributors[iter].skills[skill.name].level >= skill.level:
+            break
+        if contributors[iter].skills[skill.name].level < skill.level - 1:
+            return False
+        if find_a_mentor(contributors, skill):
+            break
+        else:
+            return False
     return True
 
 
--- teamwork2022/solvers/refined_example.py
+++ teamwork2022/solvers/refined_example.py
@@ -1,5 +1,6 @@
 from teamwork2022.problem import Solution, Problem
 import logging
+from teamwork2022.solvers.mentoring import mentor_contributors
 
 logger = logging.getLogger("example.py")
 
@@ -35,16 +36,23 @@ def solve(problem: Problem) -> Solution:
             if not learning_opportunity:
                 continue
             project_contributors = []
+            project_contributors_name = []
             for role in project.roles:
                 for contributor in contributors_by_skills[role.name]:
                     if contributor.skills[role.name].level >= role.level and contributor.name not in project_contributors:
-                        project_contributors.append(contributor.name)
+                        project_contributors_name.append(contributor.name)
+                        project_contributors.append(contributor)
                         break
 
             if len(project_contributors) == len(project.roles):
-                logger.debug(f"Planning {project.name} with {project_contributors}")
-                solution.plan_project(project.name, project_contributors)
+                logger.debug(f"Planning {project.name} with {project_contributors_name}")
+                solution.plan_project(project.name, project_contributors_name)
                 delete_keys.append(project.name)
+                learned_contributor = mentor_contributors(project_contributors, project)
+                for contributor in learned_contributor:
+                    for skill in contributor.skills.values():
+                        contributors_by_skills.setdefault(skill.name, []).append(contributor)
+
 
         for key in delete_keys:
             del problem.projects[key]
